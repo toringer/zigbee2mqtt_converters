58d0512d03d17ad557b3c8d3985ac988fbaeae0c
Tor Inge Redalen <toringe@redalen.no>
2025-11-01 18:35:20 +0100

    -

--- external_converters/schneider_eko07259.js
+++ external_converters/schneider_eko07259.js
@@ -10,26 +10,21 @@ export default {
     zigbeeModel: ['EKO07259'],
     model: 'EKO07259',
     vendor: 'Schneider Electric',
-    description: 'Automatically generated definition',
+    description: 'Smart thermostat',
     extend: [
-        m.deviceEndpoints({"endpoints":{"1":1,"2":2,"3":3,"5":5}}), 
-        m.temperature({"endpointNames":["2","3"]}), 
-        m.electricityMeter({"cluster":"metering"}),
+        m.temperature({ "endpointNames": ["2", "3"] }),
     ],
-    fromZigbee: [
-        fz.thermostat,
-        {
-            cluster: 'hvacUserInterfaceCfg',
-            type: ['attributeReport', 'readResponse'],
-            convert: (model, msg, publish, options, meta) => {
-                const attrId = 0xe001;
-                if (msg.data && (msg.data.hasOwnProperty(attrId) || msg.data.hasOwnProperty(String(attrId)))) {
-                    const value = msg.data[attrId] ?? msg.data[String(attrId)];
-                    return {inactive_brightness: value};
-                }
-            },
+    fromZigbee: [fz.stelpro_thermostat, fz.metering, fz.schneider_pilot_mode, fz.wiser_device_info, fz.hvac_user_interface, {
+        cluster: 'hvacUserInterfaceCfg',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const attrId = 0xe001;
+            if (msg.data && (msg.data.hasOwnProperty(attrId) || msg.data.hasOwnProperty(String(attrId)))) {
+                const value = msg.data[attrId] ?? msg.data[String(attrId)];
+                return { inactive_brightness: value };
+            }
         },
-    ],
+    }],
     toZigbee: [
         {
             key: ['inactive_brightness'],
@@ -50,7 +45,7 @@ export default {
                     await endpoint.write('hvacUserInterfaceCfg', payload, {
                         manufacturerCode: 0x105e,
                     });
-                    return {state: {inactive_brightness: brightness}};
+                    return { state: { inactive_brightness: brightness } };
                 } else {
                     throw new Error(`Inactive brightness value must be between 0 and 100, got ${brightness}`);
                 }
@@ -60,31 +55,31 @@ export default {
                 if (!endpoint) {
                     throw new Error('Endpoint 1 not found');
                 }
-                await endpoint.read('hvacUserInterfaceCfg', [0xe001], {manufacturerCode: 0x105e});
+                await endpoint.read('hvacUserInterfaceCfg', [0xe001], { manufacturerCode: 0x105e });
             },
         },
         tz.thermostat_occupied_heating_setpoint,
-            tz.thermostat_system_mode,
-            tz.thermostat_running_state,
-            tz.thermostat_local_temperature,
-            tz.thermostat_control_sequence_of_operation,
-            tz.schneider_pilot_mode,
-            tz.schneider_thermostat_keypad_lockout,
-            tz.thermostat_temperature_display_mode,
+        tz.thermostat_system_mode,
+        tz.thermostat_running_state,
+        tz.thermostat_local_temperature,
+        tz.thermostat_control_sequence_of_operation,
+        tz.schneider_pilot_mode,
+        tz.schneider_thermostat_keypad_lockout,
+        tz.thermostat_temperature_display_mode,
     ],
     exposes: [
         e.binary("keypad_lockout", ea.STATE_SET, "lock1", "unlock").withDescription("Enables/disables physical input on the device"),
-            e.enum("schneider_pilot_mode", ea.ALL, ["contactor", "pilot"]).withDescription("Controls piloting mode"),
-            e
-                .enum("temperature_display_mode", ea.ALL, ["celsius", "fahrenheit"])
-                .withDescription("The temperature format displayed on the thermostat screen"),
-            e
-                .climate()
-                .withSetpoint("occupied_heating_setpoint", 0, 40, 0.5)
-                .withLocalTemperature()
-                .withSystemMode(["off", "heat"])
-                .withRunningState(["idle", "heat"])
-                .withPiHeatingDemand(),
+        e.enum("schneider_pilot_mode", ea.ALL, ["contactor", "pilot"]).withDescription("Controls piloting mode"),
+        e
+            .enum("temperature_display_mode", ea.ALL, ["celsius", "fahrenheit"])
+            .withDescription("The temperature format displayed on the thermostat screen"),
+        e
+            .climate()
+            .withSetpoint("occupied_heating_setpoint", 0, 40, 0.5)
+            .withLocalTemperature()
+            .withSystemMode(["off", "heat"])
+            .withRunningState(["idle", "heat"])
+            .withPiHeatingDemand(),
         e.numeric('inactive_brightness', ea.ALL)
             .withUnit('%')
             .withValueMin(0)
